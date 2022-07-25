344f6240b35199db40b5f54039cedb088e8844b2
Emanuel Aseghehey <98903704+easeghehey@users.noreply.github.com>
2022-07-25 16:07:22 -0400

    Update best_time_to_buy_sell_stock.py

--- Dynamic Programming/best_time_to_buy_sell_stock.py
+++ Dynamic Programming/best_time_to_buy_sell_stock.py
@@ -1,23 +1,31 @@
 # The brute force approach is: 
+# Runs in O(n^2) ... bad
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(len(prices)):
-            for j in range (i, len(prices)): # starts at i, since you cannot back in days
-                  profit = max(profit, prices[j]-prices[i])
-        return profit
+def maxProfit(self, prices: List[int]) -> int:
+    profit = 0
+    for i in range(len(prices)):
+        for j in range (i, len(prices)): # starts at i, since you cannot back in days
+              profit = max(profit, prices[j]-prices[i])
+    return profit
       
-# The improvement from the 1st approach:
+# O(n) solutions:
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        l,r = 0,1
-        profit = 0
-        while r < len(prices):
-            if prices[r] > prices[l]:
-                profit = max(profit, prices[r]-prices[l])
-            else:
-                l=r
-            r+=1
-        return profit
+def maxProfit(self, prices: List[int]) -> int:
+    l,r = 0,1
+    profit = 0
+    while r < len(prices):
+        if prices[r] > prices[l]:
+            profit = max(profit, prices[r]-prices[l])
+        else:
+            l=r
+        r+=1
+    return profit
+
+# Other solution that works just as fine
+def maxProfit(prices):
+    max_profit, min_price = 0, float('inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
