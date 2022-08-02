adc432cdbcc368a084b7613e8696c82d8813a29f
easeghehey <emanuelaseghey9@gmail.com>
2022-08-01 22:29:36 -0400

    jupyter notebook notes

--- Dynamic Programming/ClimbStairs.py
+++
@@ -1,7 +0,0 @@
-    def climbStairs(self, n: int) -> int:
-        one, two = 1,1
-        for i in range(n-1):
-            tmp = one
-            one = one + two
-            two = tmp
-        return one
--- string/validAnagram.py
+++
@@ -1,8 +0,0 @@
-# Came up with this solution
-# Since they basically just need to have the same number of characters this is one way to do it
-
-# Another way is to count the frequency of every character in both strings and compare them
-
-def isAnagram(self, s: str, t: str) -> bool:
-    return sorted(s) == sorted(t)
-
