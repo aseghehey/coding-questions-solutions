3a8453bdf3a7fbfccf330985ddae14f5edbfe459
easeghehey <emanuelaseghey9@gmail.com>
2022-09-03 22:12:07 -0400

    sep 3

---
+++ Random Questions/nak.py
@@ -0,0 +1,7 @@
+# missing words
+
+
+s = "Python is an easy to learn powerful programming language It has efficient high-level data structures and a simple but effective approach to objectoriented programming Python elegant syntax and dynamic typing"
+t = "Python is an easy to learn powerful programming language"
+
+print(s.split(),'\n',t.split())
